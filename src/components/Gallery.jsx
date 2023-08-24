5cb896a1ce8bd8066696e80441beb552cb8231aa
EdwinSch <edwinschutjes@gmail.com>
2023-08-24 15:29:32 +0200

    add: react query functions

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=WppTM_KZsDllUnftHP2iTFi4PU355nGHCXTIgJ18AxY&query=dog";
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+
+  console.log(response);
   return <p>gallery</p>;
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// Styles
 import "./index.css";
 // Global context provider
 import { AppProvider } from "./context.jsx";
+// React query provider
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </AppProvider>
 );
